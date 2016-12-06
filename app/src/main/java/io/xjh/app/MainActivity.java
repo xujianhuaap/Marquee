158fa07c08d5a94df8b2653e16116bcaf5392fea
xujianhuaap <xujianhuaap@gmail.com>
2016-12-06 20:34:29 +0800

    MainActivity '

--- app/src/main/java/io/xjh/app/MainActivity.java
+++ app/src/main/java/io/xjh/app/MainActivity.java
@@ -13,56 +13,19 @@ import io.xjh.app.bean.Student;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Student> students;
+
     private MarqueeView<Student> view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        view = (MarqueeView)findViewById(R.id.view);
-        View addView=findViewById(R.id.tv_button_add);
+        setContentView(R.layout.fragment_main);
         View deleteView=findViewById(R.id.tv_button_delete);
-        students = new ArrayList<>();
-        for(int i=0;i<2;i++){
-            Student s;
-            if (i%2==0){
-                s=new Student("河出伏流，一泻汪洋。鹰隼肆翼。",23+i);
-            }else {
-                s=new Student("明知不可，而为之。",23+i);
-            }
 
-            students.add(s);
-        }
-
-        try {
-            view.setOnItemClickListener(new MarqueeView.MarqueesItemClickListener<Student>() {
-                @Override
-                public void onItemClick(Student o) {
-                    if(o!=null) Toast.makeText(MainActivity.this,o.getName()+"sss",Toast.LENGTH_SHORT).show();
-                }
-            });
-            view.setNews(students);
-            view.startScroll();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        addView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    view.setNews(students);
-                    view.startScroll();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
         deleteView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,new MainFragment()).commit();
             }
         });
     }
